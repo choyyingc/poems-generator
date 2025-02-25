674ae31a2f19f07fa457dd6deaeeebe907421a1b
ggbr <82414267+choyyingc@users.noreply.github.com>
2025-02-25 21:52:30 +0100

    Integrated with API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 function showPoem(response) {
-    response.preventDefault();
     new Typewriter("#poem", {
-        strings: 'And sorry I could not travel both',
+        strings: response.data.answer,
         autoStart: true,
         delay: 15,
         cursor: '',
     });
 }
 
-let genPoem = document.querySelector("#poem-generator");
-genPoem.addEventListener('submit', showPoem)
+function findPoem(event) {
+    event.preventDefault();
+
+    let theme = document.querySelector("#theme");
+    let key = "35aeaec3e4b1co4afd93601d25bbdft0";
+    let prompt = `Write a poem about ${theme.value}`;
+    let context = "You are a French poet and you create 4-lines poems. Please create a new poem using the keyword provided. Each poem should have only 4 lines and each line is separated using <br>. At the end of the poem, add '<strong>by SheCodes AI</strong>'.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+    console.log(`The context is ${context}`);
+    console.log(`The prompt is ${prompt}`);
+    console.log("Writing a poem for you now");
+
+    axios.get(apiUrl).then(showPoem);
+}
+
+let poemGen = document.querySelector("#poem-generator");
+poemGen.addEventListener('submit', findPoem)
